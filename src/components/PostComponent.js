766fb9d4c615edd9ae7dc186f798702b14a21d97
oussamasoui <souioussama86@gmail.com>
2020-11-20 22:48:39 +0100

    linek and solve conflect

--- src/components/PostComponent.js
+++ src/components/PostComponent.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 // import { SafeAreaView } from 'react-native-safe-area-context';
-import {View, Text, StyleSheet, SafeAreaView, ScrollView} from 'react-native'
+import {View, Text, StyleSheet, SafeAreaView, ScrollView, Share} from 'react-native'
 import { Video } from 'expo-av';
 
 
@@ -9,7 +9,27 @@ const LeftContent = () => (
   <Avatar.Image size={24} source={require('../../assets/profile-photo/me.png')} />
 );
 
-const PostComponent = () => (
+const onShare = async () => {
+  try {
+    const result = await Share.share({
+      message: 'React Native | A framework for building native apps using React',
+    });
+    if (result.action === Share.sharedAction) {
+      if (result.activityType) {
+        // shared with activity type of result.activityType
+      } else {
+        // shared
+      }
+    } else if (result.action === Share.dismissedAction) {
+      // dismissed
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+const PostComponent = () => {
+  return (
   <SafeAreaView>
     <ScrollView>
     <View style={{justifyContent: 'center'}}>
@@ -19,7 +39,7 @@ const PostComponent = () => (
   source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
   rate={1.0}
   volume={1.0}
-  isMuted={false}
+  isMuted={true}
   resizeMode="cover"
   shouldPlay
   isLooping
@@ -34,7 +54,7 @@ const PostComponent = () => (
 <Button style={styles.feed} icon={require('../../assets/profile-photo/Comment.png')}>
  Comment
 </Button>
-<Button icon={require('../../assets/profile-photo/share.png')}>
+<Button icon={require('../../assets/profile-photo/share.png')} onPress={onShare}>
  Share
 </Button>
 
@@ -48,10 +68,11 @@ const PostComponent = () => (
   source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
   rate={1.0}
   volume={1.0}
-  isMuted={false}
+  isMuted={true}
   resizeMode="cover"
   shouldPlay
   isLooping
+  
   style={styles.card}
 />
 <Card.Content>
@@ -71,13 +92,14 @@ const PostComponent = () => (
   <Card>
     <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
     <Video
-  // source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+  source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
   rate={1.0}
   volume={1.0}
-  isMuted={false}
+  isMuted={true}
   resizeMode="cover"
   shouldPlay
   isLooping
+  
   style={styles.card}
 />
 <Card.Content>
@@ -98,7 +120,8 @@ const PostComponent = () => (
   </View>
   </ScrollView>
   </SafeAreaView>
-);
+)
+  };
 
 export default PostComponent;
 
@@ -106,7 +129,7 @@ const styles = StyleSheet.create({
   feed: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   card: {
     width: 330,
--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,20 +1,51 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
-const CameraScreen = props =>{
-  return (<View style={styles.ProfileScreen}>
-    <Text>This is the CameraScreen</Text>
-  </View>)
-}; 
+export default function CameraScreen() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
 
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
 
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  if (hasPermission === null) {
+    return <View />;
   }
-});
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
-
-export default CameraScreen ;
+  return (
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} type={type}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
+            style={{
+              flex: 0.1,
+              alignSelf: 'flex-end',
+              alignItems: 'center',
+            }}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+}
