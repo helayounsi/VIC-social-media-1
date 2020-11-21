14c0d6c7b5455c5ec432122b31f1b9c71d0a2962
helayounsi <hela.ahmedyounsi@gmail.com>
2020-11-21 07:57:33 +0100

    adding search bar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import * as React from 'react';
+import {View, Text, StyleSheet, SafeAreaView, Image,
+  ScrollView, TouchableOpacity, UIManager, findNodeHandle} from 'react-native';
+import { Searchbar } from 'react-native-paper';
+
 
 const SearchScreen = props =>{
-  return (<View style={styles.ProfileScreen}>
-    <Text>This is the SearchScreen !</Text>
-  </View>)
-}; 
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const onChangeSearch = query => setSearchQuery(query);
+
+  return (
+    <View style={styles.ProfileScreen}>
+    <Searchbar
+      placeholder="Search"
+      onChangeText={onChangeSearch}
+      value={searchQuery}
+    />
+     </View>
+  );
+};
+ 
 
 
 const styles = StyleSheet.create({
