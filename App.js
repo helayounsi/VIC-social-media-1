86bf347536d53da55736802f7b8a9267bd10f54a
Nadim-Barkati <barkatinadim1994@gmail.com>
2020-11-22 19:03:57 +0100

    socket io and node express server working

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './navigation/Navigator';
-export default function App() {
+import { Component } from 'react';
+import io from 'socket.io-client';
+
+export default class App extends Component{
+  componentDidMount(){
+    const socket = io("http://localhost:19006")
+  }
+  
+  
+  render(){
   return (
       <Navigator />
   );
-};
+}
+}
