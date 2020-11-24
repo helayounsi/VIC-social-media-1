0ad21f8ca514e549b3f8110d493e2285e3a8b2e8
Nadim-Barkati <barkatinadim1994@gmail.com>
2020-11-24 11:34:24 +0100

    testing backend

--- App.js
+++ App.js
@@ -5,11 +5,7 @@ import { Component } from 'react';
 import io from 'socket.io-client';
 
 export default class App extends Component{
-  componentDidMount(){
-    const socket = io("http://localhost:19006")
-  }
-  
-  
+
   render(){
   return (
       <Navigator />
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,20 +1,52 @@
-import React from 'react';
 import {View, Text, StyleSheet} from 'react-native'
-
-const ChatScreen = props =>{
-  return (<View style={styles.ProfileScreen}>
-    <Text>This is the Chat Screen!</Text>
-  </View>)
-}; 
-
+import React, { Component } from 'react'
+import io from 'socket.io-client';
+import { TextInput } from 'react-native-paper';
+export default class ChatScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       chatMessage: "",
+       chatMessages: []
+    };
+ }
+ componentDidMount() {
+  this.socket = io("http://127.0.0.1:3000");
+   this.socket.on("chat message", msg => {
+         this.setState({ chatMessages: [...this.state.chatMessages, msg] 
+            
+    });
+    console.log(msg)
+ });
+}
+submitChatMessage() {
+  this.socket.emit('chat message', this.state.chatMessage);
+  this.setState({chatMessage: ''});
+}
+render() {
+  const chatMessages = this.state.chatMessages.map((chatMessage, index) => (
+    <Text style={{borderWidth: 2, top: 500}}>{chatMessage}</Text>
+  ))
+return(
+  <View style={styles.container}>
+{chatMessages}
+<TextInput 
+style={{height: 40, borderWidth: 2, top: 600}}
+autoCorrect={false}
+value={this.state.chatMessage}
+onSubmitEditing={() => this.submitChatMessage()}
+onChangeText={chatMessage =>{
+  this.setState({chatMessage});
+}}
+/>
+  </View>
+)
+}};
 
 const styles = StyleSheet.create({
-  screen: {
+  container: {
+    height: 400,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
+    backgroundColor: '#F5FCFF',
+  },
 });
-
-
-export default ChatScreen ;
