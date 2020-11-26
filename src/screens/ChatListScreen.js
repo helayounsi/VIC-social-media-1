0c4cdf00c10429ae11eedd6bfdbb2277af9e4697
Nadim-Barkati <barkatinadim1994@gmail.com>
2020-11-26 15:59:58 +0100

    messages list

--- src/screens/ChatListScreen.js
+++ src/screens/ChatListScreen.js
@@ -1,21 +1,56 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
 import PropTypes from 'prop-types'
 import ChatScreen from './ChatScreen'
-import { View } from 'react-native-animatable'
 import { List } from 'react-native-paper'
-import { FlatList } from 'react-native-gesture-handler'
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, TouchableOpacity } from 'react-native';
+import DATA from './DummyMessages'
 
-export default class ChatListScreen extends Component {
-    static propTypes = {
-        prop: PropTypes
-    }
-
-    render() {
-        return (
-            <View>
-            <FlatList>sdsdsdsds</FlatList>
-            </View>
-            
-        )
-    }
-}
+const Item = ({ item, onPress, style }) => (
+    <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+      <Text style={styles.message}>{item.message}</Text>
+    </TouchableOpacity>
+  );
+  
+  const ChatListScreen = () => {
+    const [selectedId, setSelectedId] = useState(null);
+  
+    const renderItem = ({ item }) => {
+      const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+  
+      return (
+        <Item
+          item={item}
+          onPress={() => setSelectedId(item.id)}
+          style={{ backgroundColor }}
+        />
+      );
+    };
+  
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          extraData={selectedId}
+        />
+      </SafeAreaView>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    message: {
+      fontSize: 32,
+    },
+  });
+  
+  export default ChatListScreen;
