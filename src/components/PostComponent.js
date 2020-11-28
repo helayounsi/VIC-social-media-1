2fe91b8d3481de5bd36f175ce53d81401ac19ae5
helayounsi <hela.ahmedyounsi@gmail.com>
2020-11-28 04:23:22 +0100

    adding map to iterate throught tha array and return each link in the card

--- src/components/PostComponent.js
+++ src/components/PostComponent.js
@@ -34,21 +34,21 @@ class PostComponent extends Component {
     super(props);
     this.state={
       media: ["https://i2.wp.com/www.alphr.com/wp-content/uploads/2018/04/how_to_back_up_photos_on_google_photos.jpg?zoom=2&resize=738%2C320", "https://bloximages.chicago2.vip.townnews.com/mymcr.net/content/tncms/assets/v3/editorial/a/6c/a6c39bd0-b325-11ea-9027-334715b6d420/5eee587f1da77.image.jpg?resize=1200%2C922","https://cdn.pizap.com/pizapfiles/images/photo_effects_filters_app05.jpg", "https://photolemur.com/img/home/top-slider/after-1440.jpg","https://photolemur.com/uploads/blog/unnamed.jpg","http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4", "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4", "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"],
-      mediaList:""
     }
-
-
-
   }
-
+//  mediaList=()=>{
+//    return this.state.media.map(item=>{
+    
+//    })
+//  }
   render(){
-    mediaList=media.map(media => {mediaList})
-    console.log(mediaList);
-    if(this.state.mediaList.includes('.jpg')||mediaList.includes('.jpeg')||mediaList.includes('.png')||mediaList.includes('.gif')){
+    return this.state.media.map(item=>{
+      console.log(item)
+    if(item.includes('.jpg')||item.includes('.jpeg')||item.includes('.png')||item.includes('.gif')){
       return(
     <Card>
     <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
-    <Card.Cover source={{uri: this.state.mediaList}} />
+    <Card.Cover key={item.id} source={{uri:item}} />
 <Card.Content>
 <View style={styles.feed}>
 <Button style={styles.feed} icon={require('../../assets/profile-photo/like.png')} color={'#189ad3'}>
@@ -73,7 +73,7 @@ class PostComponent extends Component {
     <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
     
     <Video
-  source={{ uri: this.state.mediaList}}
+  key={item.id} source={{ uri: item}}
   rate={1.0}
   volume={1.0}
   isMuted={true}
@@ -104,6 +104,7 @@ class PostComponent extends Component {
   </SafeAreaView>
 )
   }
+})
 }
   };
 
