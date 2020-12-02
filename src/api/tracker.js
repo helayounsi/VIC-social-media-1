838f3b1cfff8b303efbc2751ba2dea2040a53b97
Nadim-Barkati <barkatinadim1994@gmail.com>
2020-12-02 11:15:29 +0100

    tracker

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL:'https://vic-corporation.herokuapp.com/'
+    baseURL:'http://192.168.11.251:3000/'
 })
--- src/components/PostComponent.js
+++ src/components/PostComponent.js
@@ -491,7 +491,7 @@ const styles = StyleSheet.create({
   },
   textInput: {
     flex: 1,
-    marginTop: Platform.OS === "ios" ? 0 : -12,
+    // marginTop: Platform.OS === "ios" ? 0 : -12,
     paddingLeft: 10,
     color: "#05375a",
   },
